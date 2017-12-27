33cc86d73855e246f38dab7c140fbb4bc51ff739
Franco Rabec <franco.rabec@hotmail.fr>
2017-12-27 04:01:15 +0100

    Taches 1 et 2 debuggées reste à essayer tous les tests et voir si le constructeur détecte les grilles invalides

--- src/Game.java
+++ src/Game.java
@@ -3,16 +3,16 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Game {
-	public int size; //size of the grid 
+	public int size; // size of the grid
 	public int nbVehicles; // number of vehicles
-	public int [][] grid;
-	
-	public Game (int [][] grid, int size, int nbVehicles){
-		
+	public int[][] grid;
+
+	public Game(int[][] grid, int size, int nbVehicles) {
+
 	}
-	
-	public Game (String fileName) throws FileNotFoundException{
-		File fichier  = new File (fileName);
+
+	public Game(String fileName) throws FileNotFoundException {
+		File fichier = new File(fileName);
 		java.util.Scanner lecteur = new Scanner(fichier);
 		int size;
 		int nbVehicles;
@@ -20,47 +20,50 @@ public class Game {
 		int currentSize;
 		int currentx;
 		int currenty;
-		size=lecteur.nextInt();
-		nbVehicles=lecteur.nextInt();
-		int[][] grid = new int [size][size];
-		for (int i =0; i<nbVehicles;i++){
-			for (int j =0; j<5;j++){
-				currentVehicle=lecteur.nextInt();
-				if (lecteur.next()=="h"){
-					currentSize=lecteur.nextInt();
-					currentx=lecteur.nextInt();
-					currenty=lecteur.nextInt();
-					for (int v =0; v<currentSize;v++){
-						if (grid[currentx][currenty + v]!=0){
-							throw new IllegalArgumentException();
-						}
-						
-						else {
-							grid[currentx][currenty + v]=currentVehicle;
-						}
+		size = lecteur.nextInt();
+		nbVehicles = lecteur.nextInt();
+		int[][] grid = new int[size][size];
+		for (int i = 0; i < nbVehicles; i++) {
+			currentVehicle = lecteur.nextInt();
+			if (lecteur.next().equals("h")) {
+				currentSize = lecteur.nextInt();
+				currentx = lecteur.nextInt();
+				currenty = lecteur.nextInt();
+				for (int v = 0; v < currentSize; v++) {
+					if (grid[currenty - 1][currentx - 1 + v] != 0) {
+						throw new IllegalArgumentException();
+					}
+
+					else {
+						grid[currenty - 1][currentx - 1 + v] = currentVehicle;
 					}
 				}
-				else{
-					currentSize=lecteur.nextInt();
-					currentx=lecteur.nextInt();
-					currenty=lecteur.nextInt();
-					for (int v =0; v<currentSize;v++){
-						if (grid[currentx+v][currenty]!=0){
-							throw new IllegalArgumentException();
-						}
-						else {
-							grid[currentx+v][currenty]=currentVehicle;
-						}
+			} else {
+				currentSize = lecteur.nextInt();
+				currentx = lecteur.nextInt();
+				currenty = lecteur.nextInt();
+				for (int v = 0; v < currentSize; v++) {
+					if (grid[currenty - 1 + v][currentx - 1] != 0) {
+						throw new IllegalArgumentException();
+					} else {
+						grid[currenty - 1 + v][currentx - 1] = currentVehicle;
 					}
 				}
 			}
 		}
 		lecteur.close();
-		this.grid=grid;
-		this.size=size;
-		this.nbVehicles=nbVehicles;
-		
-		
+		this.grid = grid;
+		this.size = size;
+		this.nbVehicles = nbVehicles;
 	}
 	
+	public void display(){
+		for (int i =0; i<grid.length; i++){
+			for (int j=0 ; j<grid.length; j++){
+				System.out.print(grid[i][j] +" ");
+			}
+			System.out.println("");
+		}
+	}
+
 }
