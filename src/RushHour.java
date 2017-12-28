0005cf837fe3d10381d6d6c501a0a6a858bf9075
Jérémy Marquis <34447092+JrmyM@users.noreply.github.com>
2017-12-28 13:47:21 +0100

    Sans bug c'est mieux
    J'en ai profité pour utiliser ta manière de lire le fichier d'entrée, avec Scanner. C'est vrai que c'est beaucoup plus simple...

--- src/RushHour.java
+++ src/RushHour.java
@@ -1,6 +1,8 @@
 /* Class containing all the programs asked in the PI project */
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class RushHour {
 
@@ -9,20 +11,15 @@ public class RushHour {
 	int size;
 	Vehicle[] vehicules;
 	
-	RushHour(String File) throws IOException {
-		BufferedReader br;
-		br = new BufferedReader(new FileReader(File));
-		String line;
-		line = br.readLine();
-		this.size = (int) line.charAt(0);
-		line = br.readLine();
-		int n = (int) line.charAt(0);
-		this.vehicules = new Vehicle[n];
-		for (int i=0; i<n; i++) {
-			line = br.readLine();
-			this.vehicules[i] = new Vehicle(line);
+	RushHour(String fileName) throws FileNotFoundException {
+		File fichier = new File(fileName);
+		java.util.Scanner lecteur = new Scanner(fichier);
+		this.size = lecteur.nextInt();
+		this.vehicules = new Vehicle[lecteur.nextInt()];
+		for (int i = 0; i<vehicules.length; i++) {
+			vehicules[i] = new Vehicle(lecteur.nextInt(), lecteur.next(), lecteur.nextInt(), lecteur.nextInt(), lecteur.nextInt());	
 		}
-		br.close();
+		lecteur.close();
 	}
 	
 ///// Method used to display a Rush Hour Grid /////
@@ -32,14 +29,14 @@ public class RushHour {
 		for (Vehicle v:this.vehicules) {
 			if(v.getOrientation()=='h') {
 				for (int i=0; i<v.getLength(); i++){
-					if (tableau[v.getAbsissa()-1][v.getOrdinate()+i-1]!=0) throw new SuperpositionError();
-					tableau[v.getAbsissa()-1][v.getOrdinate()+i-1] = v.name;
+					if (tableau[v.getOrdinate()-1][v.getAbsissa()+i-1]!=0) throw new SuperpositionError();
+					tableau[v.getOrdinate()-1][v.getAbsissa()+i-1] = v.name;
 				}
 			}
 			else {
 				for (int i=0; i<v.getLength(); i++){
-					if (tableau[v.getAbsissa()+i-1][v.getOrdinate()-1]!=0) throw new SuperpositionError();
-					tableau[v.getAbsissa()+i-1][v.getOrdinate()-1] = v.name;
+					if (tableau[v.getOrdinate()+i-1][v.getAbsissa()-1]!=0) throw new SuperpositionError();
+					tableau[v.getOrdinate()+i-1][v.getAbsissa()-1] = v.name;
 				}
 			}
 		}
