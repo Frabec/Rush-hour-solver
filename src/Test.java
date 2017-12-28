9af13cc72509472e47b4909006766fa3053d5de5
Jérémy Marquis <34447092+JrmyM@users.noreply.github.com>
2017-12-28 15:13:59 +0100

    Test avec move
    Histoire de voir comment la méthode move de la classe Vehicle fonctionne

--- src/Test.java
+++ src/Test.java
@@ -11,5 +11,15 @@ public class Test {
 			 	}
 			 	System.out.println("");
 			 }
+		System.out.println("");
+		R.vehicules[6].move(1);   //// Check that the car 7 is moving the right way.
+		R.vehicules[1].move(2);
+		tableau = R.display();
+		for (int i =0; i<tableau.length; i++){
+		 	for (int j=0 ; j<tableau.length; j++){
+		 		System.out.print(tableau[i][j] +" ");
+		 	}
+		 	System.out.println("");
+		 }
 	}
 }
