0bb7902743583a2d80e7fe7f42b6553fecdb2b7b
Jérémy Marquis <34447092+JrmyM@users.noreply.github.com>
2017-12-28 13:45:21 +0100

    En débuggé c'est mieux

--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,14 @@ import java.io.IOException;
 
 public class Test {
 
-	public static void main() throws IOException {
+	public static void main(String[] args) throws IOException, SuperpositionError {
 		RushHour R = new RushHour("RushHour1.txt");
-		System.out.println(R.display());
+		int[][] tableau = R.display();
+		for (int i =0; i<tableau.length; i++){
+			 	for (int j=0 ; j<tableau.length; j++){
+			 		System.out.print(tableau[i][j] +" ");
+			 	}
+			 	System.out.println("");
+			 }
 	}
 }
