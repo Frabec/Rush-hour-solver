42f5a30e42e34e2cfaf3a57d3e93f594e4440e82
Jérémy Marquis <34447092+JrmyM@users.noreply.github.com>
2017-12-28 10:32:57 +0100

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -3,9 +3,7 @@ import java.io.IOException;
 public class Test {
 
 	public static void main() throws IOException {
-		//RushHour R = new RushHour("RushHour1.txt");
-		//System.out.println(R.display());
-		int[][] tableau = new int[3][3];
-		System.out.println(tableau);
+		RushHour R = new RushHour("RushHour1.txt");
+		System.out.println(R.display());
 	}
 }
