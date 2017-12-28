c1043fa08b0f2d8168fe61e7c4b31b2a6671d1d5
Jérémy Marquis <34447092+JrmyM@users.noreply.github.com>
2017-12-28 22:12:34 +0100

    Test des fonctions ajoutées à RushHour

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Test {
 
 	public static void main(String[] args) throws IOException, SuperpositionError {
-		RushHour R = new RushHour("RushHour1.txt");
+		RushHour R = new RushHour("RushHour5.txt");
 		int[][] tableau = R.display();
 		for (int i =0; i<tableau.length; i++){
 			 	for (int j=0 ; j<tableau.length; j++){
@@ -12,14 +12,9 @@ public class Test {
 			 	System.out.println("");
 			 }
 		System.out.println("");
-		R.vehicules[6].move(1);   //// Check that the car 7 is moving the right way.
-		R.vehicules[1].move(2);
-		tableau = R.display();
-		for (int i =0; i<tableau.length; i++){
-		 	for (int j=0 ; j<tableau.length; j++){
-		 		System.out.print(tableau[i][j] +" ");
-		 	}
-		 	System.out.println("");
-		 }
+		System.out.println(RushHour.AllPossibleMoves(R));
+		System.out.println(RushHour.AllPossibleMoves(R).size());
+		System.out.println("R est résolu : " + R.isSolution());
+		System.out.println(RushHour.BFS(R));
 	}
 }
